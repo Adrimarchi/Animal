b41dda00eb70d400c277f584468c565e77466294
Daniel <daniel.marchi@portoseguro.com.br>
2020-07-21 09:57:48 -0300

    boi

---
+++ Boi.cs
@@ -0,0 +1,11 @@
+namespace Animal
+{
+    public class Boi : Animal
+    {
+        public Boi()
+        {
+            Tipo = "boi";
+            TempoDoPasso = 1500;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,25 +7,27 @@ namespace Animal
         static void Main(string[] args)
         {
             Console.WriteLine("Qual animal você quer?");
-            var animal = Console.ReadLine();
-            var alface = new Alface();
+            var tipoDeAnimal = Console.ReadLine();
+            var animal = CriarAnimal(tipoDeAnimal);
 
-            if(animal == "tartaruga"){
-                var tartaruga = new Tartaruga();
-                tartaruga.Andar();
-                tartaruga.Comer(alface.Tipo);
-            }
-            else if(animal == "cachorro"){
-                var cachorro = new Cachorro();
-                cachorro.Andar();
-                cachorro.Comer(alface.Tipo);
-            }
-            else if(animal == "leopardo"){
-                var leopardo = new Leopardo();
-                leopardo.Andar();
-                leopardo.Comer(alface.Tipo);
-            } else {
-                Console.WriteLine("Não conheço esse animal");
+            animal.Andar();
+            animal.Comer(new Alface().Tipo);
+        }
+
+        static Animal CriarAnimal(string tipoDeAnimal)
+        {
+            switch(tipoDeAnimal)
+            {
+                case "boi":
+                    return new Boi();
+                case "tartaruga":
+                    return new Tartaruga();
+                case "cachorro":
+                    return new Cachorro();
+                case "leopardo":
+                    return new Leopardo();
+                default:
+                    throw new Exception("Não conheço esse animal");
             }
         }
     }
